5f6f6c85f441dae81043b1550b0b5cae7d5725b6
stansakl <stansakl@users.noreply.github.com>
2018-09-06 23:21:08 -0500

    Stub out Functional Architecture project

---
+++ Functional/KWICFunctional/src/kwic/Main.java
@@ -0,0 +1,18 @@
+package kwic;
+
+/**
+ *
+ * @author ssakl
+ */
+public class Main 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("Starting Keyword in Context processing.");
+    }
+    
+}
