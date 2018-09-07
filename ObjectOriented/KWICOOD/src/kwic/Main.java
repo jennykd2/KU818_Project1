449f360728755193fb100eb2965b5cee070bad2e
stansakl <stansakl@users.noreply.github.com>
2018-09-06 23:54:47 -0500

    Stub out OOD project.

---
+++ ObjectOriented/KWICOOD/src/kwic/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package kwic;
+
+/**
+ *
+ * @author ssakl
+ */
+public class Main 
+{
+    public Main(String filename)
+    {
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        Main main = new Main(args[0]);
+    }
+
+}
