4158825d29c258622c49a2ea5899500c2696f502
stansakl <stansakl@users.noreply.github.com>
2018-09-06 23:37:29 -0500

    Start stubbing out methods.

--- Functional/KWICFunctional/src/kwic/Main.java
+++ Functional/KWICFunctional/src/kwic/Main.java
@@ -1,18 +1,39 @@
 package kwic;
 
+import java.io.*;
+import static java.lang.System.out;
 /**
- *
- * @author ssakl
+ * This class demonstrates a functional architecture of the 
+ * Keyword in Context.
+ * 
+ * @author Stan Sakl
+ * @author Jennifer Booth
  */
 public class Main 
 {
-
     /**
-     * @param args the command line arguments
+     * @param args A single string containing the input file name.
      */
     public static void main(String[] args) 
     {
-        System.out.println("Starting Keyword in Context processing.");
+        out.println("Starting Keyword in Context processing.");
+        
+        if (args.length != 1)
+        {
+            out.println("You must provide a single argument containing the file name. Program will now exit.");
+            System.exit(-1);
+        }
+        
+        openInputFile(args[0]);
+    }
+    
+    private static boolean openInputFile(String filename)
+    {
+        File f = new File(filename);
+        out.println("Attempting to open " + f.getName());
+        boolean isFileOpen = false;
+        
+        return isFileOpen;
     }
     
 }
