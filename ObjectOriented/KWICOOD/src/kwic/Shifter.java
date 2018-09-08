0f23663af9c860e962fe17628a526916fdeaaeb7
stansakl <stansakl@users.noreply.github.com>
2018-09-07 20:56:45 -0500

    Work on classes to shift words.

--- ObjectOriented/KWICOOD/src/kwic/Shifter.java
+++ ObjectOriented/KWICOOD/src/kwic/Shifter.java
@@ -1,13 +1,41 @@
 package kwic;
 
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 /**
  *
  * @author ssakl
  */
-public class Shifter 
+public class Shifter implements IShifter 
 {
+    private ArrayList<String> wordList = new ArrayList<>();
+    
     public Shifter()
     {
         
+    }
+
+    @Override
+    public ArrayList<String> getWordList()
+    {
+        return wordList;
+    }
+
+    @Override
+    public void generateWordListFromLineList(ArrayList<String> lineList)
+    {
+        String line = "";
+        StringTokenizer tokenizer;
+        for(int i = 0; i < lineList.size(); i++)
+        {
+            line = lineList.get(i);
+            tokenizer = new StringTokenizer(line, " ");
+            
+            while(tokenizer.hasMoreElements())
+            {
+                this.wordList.add(tokenizer.nextToken());
+            }
+        }
     }
 }
