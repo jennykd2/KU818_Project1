4925aaf6e49104f79099e930fd3c5580ac51a18f
stansakl <stansakl@users.noreply.github.com>
2018-09-09 09:19:34 -0500

    Initial commit

---
+++ PipeAndFilter/ParseKeywordFile/src/kwicpipes/ParseKeywordFile.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package kwicpipes;
+
+/**
+ * This program parses lines out of an input file and outputs the result
+ * @author ssakl
+ */
+public class ParseKeywordFile 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+    }
+
+}
